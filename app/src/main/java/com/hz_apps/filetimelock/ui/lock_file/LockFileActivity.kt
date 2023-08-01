f1de02ce7db1012ff8a79f0ea1b09b5f9fffb078
Hammad Zafar Bawara <hammadbawara@gmail.com>
2023-08-01 12:39:53 +0500

    lockfile get added

--- app/src/main/java/com/hz_apps/filetimelock/ui/lock_file/LockFileActivity.kt
+++ app/src/main/java/com/hz_apps/filetimelock/ui/lock_file/LockFileActivity.kt
@@ -2,24 +2,34 @@ package com.hz_apps.filetimelock.ui.lock_file
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import com.hz_apps.filetimelock.R
+import com.hz_apps.filetimelock.databinding.ActivityLockFileBinding
 import com.hz_apps.filetimelock.ui.file_picker.FilePickerActivity
+import java.io.File
 
 class LockFileActivity : AppCompatActivity() {
 
-
-
+    private lateinit var lockFile : File
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lock_file)
+        val bindings = ActivityLockFileBinding.inflate(layoutInflater)
+        setContentView(bindings.root)
 
         val startForResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val intent = result.data
+                lockFile = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    intent!!.getSerializableExtra("result", File::class.java)!!
+                }else{
+                    intent!!.getSerializableExtra("result") as File
+                }
+
+                bindings.filenameLockFile.text = lockFile.name
+
             }else{
                 finish()
             }
