c4fabd81238f5d30344c2c777da39ee815973077
Hammad Zafar Bawara <hammadbawara@gmail.com>
2023-08-19 14:56:30 +0500

    storage permission fixed

--- app/src/main/java/com/hz_apps/filetimelock/dialogs/FileTransferDialog.kt
+++ app/src/main/java/com/hz_apps/filetimelock/dialogs/FileTransferDialog.kt
@@ -9,6 +9,7 @@ import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.webkit.MimeTypeMap
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.DialogFragment
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -18,18 +19,25 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
-import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
 
 class FileTransferDialog: DialogFragment() {
 
     private val bindings : DialogCopyFileBinding by lazy {
         DialogCopyFileBinding.inflate(layoutInflater)
     }
+    private lateinit var mainDialog : Dialog
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val builder = MaterialAlertDialogBuilder(requireContext())
 
         builder.setView(bindings.root)
         builder.setCancelable(false)
+        builder.setNegativeButton("Cancel"
+        ) { dialog, which ->
+            dismiss()
+        }
 
         val source = arguments?.getString("source")
         val destination = arguments?.getString("destination")
@@ -51,20 +59,38 @@ class FileTransferDialog: DialogFragment() {
             }
         }
 
-        return builder.create()
+        mainDialog = builder.create()
+        return mainDialog
     }
 
-    @RequiresApi(Build.VERSION_CODES.Q)
-    private suspend fun transferFile(source: File, contentResolver: ContentResolver) {
-        if (!source.exists()) {
-            throw FileNotFoundException("Source file does not exist.")
+    private fun onCopyError(message: String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+            mainDialog.dismiss()
         }
+    }
 
+    private fun checkSourceAndDirectory(source: File) : File{
+        if (!source.exists()) {
+            onCopyError("Source File doesn't exists")
+        }
         val downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
         val timeLockFolder = File(downloadDir, "File Time Lock")
         if (!timeLockFolder.exists()) {
             timeLockFolder.mkdirs()
+            if (!timeLockFolder.mkdirs()) {
+                onCopyError("Unable to make directory")
+            }
+
         }
+
+        return timeLockFolder
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private suspend fun transferFile(source: File, contentResolver: ContentResolver) {
+
+        val timeLockFolder = checkSourceAndDirectory(source)
 
         val destinationFileName = source.name
 
@@ -105,7 +131,7 @@ class FileTransferDialog: DialogFragment() {
             contentValues.put(MediaStore.Downloads.IS_PENDING, 0)
             contentResolver.update(uri, contentValues, null, null)
         } catch (e: Exception) {
-            e.printStackTrace()
+            throw Exception("Something went wrong")
         }
     }
 
@@ -122,43 +148,66 @@ class FileTransferDialog: DialogFragment() {
     }
 
     private suspend fun transferFileBelowAndroid10(source: File, contentResolver: ContentResolver) {
-        if (!source.exists()) {
-            throw FileNotFoundException("Source file does not exist.")
-        }
 
-        val downloadDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
-        val timeLockFolder = File(downloadDir, "File Time Lock")
-        if (!timeLockFolder.exists()) {
-            timeLockFolder.mkdirs()
-        }
+        val timeLockFolder = checkSourceAndDirectory(source)
 
+        // Determine the destination file's name
         val destinationFileName = source.name
 
+        // Handle cases where a file with the same name already exists
         val existingFile = File(timeLockFolder, source.name)
         if (existingFile.exists()) {
             val newName = findUniqueFileName(timeLockFolder, destinationFileName)
             existingFile.renameTo(File(timeLockFolder, newName))
         }
 
+        // Create and write to the new destination file
         val newFile = File(timeLockFolder, destinationFileName)
-        newFile.outputStream().use { outputStream ->
-            source.inputStream().use { inputStream ->
-                val buffer = ByteArray(512000)
-                var bytesRead: Int
-                var totalByteRead = 0L
-                val fileSize = source.length()
+        var inputStream : InputStream? = null
+        var outputStream : OutputStream? = null
+        try {
+            inputStream = source.inputStream()
+            outputStream = newFile.outputStream()
 
-                while (inputStream.read(buffer).also { bytesRead = it } != -1) {
-                    totalByteRead += bytesRead
-                    withContext(Dispatchers.Main) {
-                        bindings.copyFileProgressBar.progress = (totalByteRead * 100 / fileSize).toInt()
-                    }
-                    outputStream.write(buffer, 0, bytesRead)
+            val buffer = ByteArray(512000)
+            var length : Int
+            val totalSize = source.length()
+            var readBytes :Int = 0
+
+            while (inputStream.read(buffer).also { length = it } > 0) {
+                outputStream.write(buffer, 0, length)
+                readBytes += length
+                val percentage = ((readBytes*100)/totalSize).toInt()
+                withContext(Dispatchers.Main){
+                    bindings.copyFileProgressBar.progress = percentage
+                    bindings.percentageCopyFileDialog.text = "$percentage%"
                 }
-                outputStream.flush()
+            }
+
+            outputStream.flush()
+
+        }
+        catch (e : Exception) {
+            onCopyError("Something went wrong")
+        }
+        finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close()
+                }catch (e : IOException) {
+                    e.printStackTrace()
+                }
+            }
+            if (outputStream != null) {
+                try {
+                    outputStream.close()
+                }catch (e: IOException){
+                    e.printStackTrace()
+                }
             }
         }
     }
+
 
 
 }
